683f17ca25cecac1878df76774721a3b62f44404
baka-gourd <36119339+baka-gourd@users.noreply.github.com>
2024-03-18 12:18:46 +0800

    maintain: update commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,5 +1,19 @@
 module.exports = {
   prompt: {
+    types: [
+      {
+        value: "origin",
+        name: "origin:     Add original language file",
+      },
+      {
+        value: "translate",
+        name: "translate:     Add translated language file",
+      },
+      {
+        value: "maintain",
+        name: "maintain:     Maintain the repo",
+      },
+    ],
     scopes: [
       { value: "zh-HANS", name: "简体中文" },
       { value: "en-US", name: "English (United States)" },
